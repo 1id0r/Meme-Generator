8ebe9df90c9fe83e51c7d01b89e399173fa3021c
1id0r <lidornissim44@gmail.com>
2024-09-25 13:44:50 +0300

    render photo with text

---
+++ js/meme.controller.js
@@ -0,0 +1,32 @@
+'use strict'
+
+var gElCanvas
+var gCtx
+
+function onInit() {
+  gElCanvas = document.querySelector('canvas')
+  gCtx = gElCanvas.getContext('2d')
+  renderMeme()
+}
+
+function renderMeme() {
+  const imgSrc = 'img/1.jpg' // Path to the image file
+  const memeText = 'I sometimes eat Falafel' // The text to be drawn
+  const textSize = 40 // Text size in pixels
+  const textColor = 'red' // Text color
+
+  // Create a new image object
+  const img = new Image()
+  img.src = imgSrc
+
+  img.onload = function () {
+    gElCanvas.width = img.width
+    gElCanvas.height = img.height
+    gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height)
+    gCtx.font = `${textSize}px Arial`
+    gCtx.fillStyle = textColor
+    gCtx.textAlign = 'center'
+
+    gCtx.fillText(memeText, gElCanvas.width / 2, 50)
+  }
+}
